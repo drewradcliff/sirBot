0cee0a7d668e3389aaed64c7c151a8d21fe391a3
Drew Radcliff <sirampr@gmail.com>
2020-02-12 10:48:14 -0500

    add discord bot

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const reddit = require("./redditapi.js");
+
+const bot = new Discord.Client();
+
+require("dotenv").config();
+
+const PREFIX = "!";
+
+bot.on("ready", () => {
+  console.log("Bot online");
+});
+
+bot.on("message", message => {
+  let args = message.content.substring(PREFIX.length).split(" ");
+
+  switch (args[0]) {
+    case "reddit":
+      reddit.topSubreddit(args[1]).then(results => {
+        message.channel.send(results[0].url);
+      });
+      break;
+    case "reddit-search":
+      reddit.search(args[1]).then(results => {
+        message.channel.send(results[0].url);
+      });
+  }
+});
+
+bot.login(process.env.API_KEY);
